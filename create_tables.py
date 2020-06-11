ecc6758e44e79736f4ad8d3b0d5bfb86be9234e9
Halah Alabbad <38733409+as234545@users.noreply.github.com>
2020-06-11 21:16:31 +0300

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -9,7 +9,7 @@ def create_database():
     """
     
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=postgres user=postgres password=postgres")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=postgres user=[] password=[]")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
